13da5716ff4ab2ab95eb507c15485513fa70badf
Adejaime Junior <39809065+adejaimejr@users.noreply.github.com>
2024-08-20 23:35:41 -0400

    Update log-archive-interactive.sh

--- log-archive-interactive.sh
+++ log-archive-interactive.sh
@@ -10,11 +10,12 @@ prompt_for_input() {
 while true; do
     echo "1. Specify Log Directory"
     echo "2. Specify Number of Days to Keep Logs"
-    echo "3. Run Log Archiving Process"
-    echo "4. Exit"
+    echo "3. Specify Number of Days to Keep Backup Archives"
+    echo "4. Run Log Archiving Process"
+    echo "5. Exit"
     echo ""
 
-    read -r -p "Choose an option [1-4]: " choice
+    read -r -p "Choose an option [1-5]: " choice
 
     case $choice in
         1)
@@ -27,10 +28,14 @@ while true; do
             fi
             ;;
         2)
-            days_to_keep=$(prompt_for_input "How many days of logs do you want to keep?" "7")
-            echo "Logs older than $days_to_keep days will be archived."
+            days_to_keep_logs=$(prompt_for_input "How many days of logs do you want to keep?" "7")
+            echo "Logs older than $days_to_keep_logs days will be archived."
             ;;
         3)
+            days_to_keep_backups=$(prompt_for_input "How many days of backup archives do you want to keep?" "30")
+            echo "Backup archives older than $days_to_keep_backups days will be deleted."
+            ;;
+        4)
             if [ -z "$log_dir" ]; then
                 echo "Error: Log directory is not set. Please set it first."
             else
@@ -43,23 +48,27 @@ while true; do
                 archive_file="$archive_dir/logs_archive_$timestamp.tar.gz"
 
                 # Find and compress logs older than specified days
-                find "$log_dir" -type f -mtime +$days_to_keep -print0 | tar -czvf "$archive_file" --null -T -
+                find "$log_dir" -type f -mtime +$days_to_keep_logs -print0 | tar -czvf "$archive_file" --null -T -
 
                 # Log the archiving process
                 echo "Logs archived in $archive_file on $(date)" >> "$archive_dir/archive_log.txt"
 
                 # Delete logs older than the specified number of days
-                find "$log_dir" -type f -mtime +$days_to_keep -exec rm -f {} \;
+                find "$log_dir" -type f -mtime +$days_to_keep_logs -exec rm -f {} \;
 
                 echo "Archiving completed: $archive_file"
+
+                # Delete backup archives older than specified days
+                find "$archive_dir" -type f -name "*.tar.gz" -mtime +$days_to_keep_backups -exec rm -f {} \;
+                echo "Backup archives older than $days_to_keep_backups days have been deleted."
             fi
             ;;
-        4)
+        5)
             echo "Exiting..."
             break
             ;;
         *)
-            echo "Invalid option. Please choose a number between 1 and 4."
+            echo "Invalid option. Please choose a number between 1 and 5."
             ;;
     esac
 done
